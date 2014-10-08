07f49025f3b961650fc43ac96c5e500460cee4a0
Thomas Preece <github@thomaspreece.com>
2014-10-08 12:22:54 +0100

    Added comments and cleaned up code

--- lib/STLExtract.rb
+++ lib/STLExtract.rb
@@ -9,24 +9,37 @@ class StlExtract
 		@y_value = -1
 		@z_value = -1
 		@volume = -1  
+		@error = -1
 		#Find gem location
 		gemPath = File.expand_path('../../', __FILE__)	
-		#Open slic3r with filename supplied
-		
-		#Mac\Slic3r.app\Contents\MacOS\slic3r
-		puts RUBY_PLATFORM
+
 		if OS.windows?
-			
+			#Open slic3r with filename supplied
 			io = IO.popen(gemPath+'/Slic3r/Win/slic3r-console.exe --info '+file+" 2>&1")
 		elsif OS.mac?
-			puts File.chmod(0777,gemPath+'/Slic3r/Mac/Slic3r.app/Contents/MacOS/slic3r')
-			io = IO.popen(gemPath+'/Slic3r/Mac/Slic3r.app/Contents/MacOS/slic3r --info '+file+" 2>&1")
+			#Set file to be executable
+			if File.chmod(0744,gemPath+'/Slic3r/Mac/Slic3r.app/Contents/MacOS/slic3r')
+				#Open slic3r with filename supplied
+				io = IO.popen(gemPath+'/Slic3r/Mac/Slic3r.app/Contents/MacOS/slic3r --info '+file+" 2>&1")
+			else
+				#RuntimeError
+				raise "Could not make Slic3r executable"
+			end
 		elsif OS.linux?
-			puts File.chmod(0777,gemPath+'/Slic3r/Linux/bin/slic3r')
-			io = IO.popen(gemPath+'/Slic3r/Linux/bin/slic3r --info '+file+" 2>&1")
+			#Open slic3r with filename supplied
+			if File.chmod(0744,gemPath+'/Slic3r/Linux/bin/slic3r')
+				#Execute file
+				io = IO.popen(gemPath+'/Slic3r/Linux/bin/slic3r --info '+file+" 2>&1")
+			else
+				#RuntimeError
+				raise "Could not make Slic3r executable"				
+			end
 		else
-			#RuntimeError?
+			#RuntimeError
+			raise "Could not detect platform from: "+RUBY_PLATFORM
 		end
+		
+		
 		
 		io.each do |s|
 			#If output contains size, extract size information using regex
@@ -45,7 +58,22 @@ class StlExtract
 					@volume = volMatch.values_at( 1 )[0]
 				end			  
 			end
-			puts s
+			
+			#Check Slic3r output for errors
+			if s.include? "Input file must have .stl, .obj or .amf(.xml) extension"
+				@error = "Filetype not STL, OBJ or AMF "+file
+			end
+			if s.include? "Failed to open"
+				@error = "Could not find: "+file
+			end 
+			if s.include? "The file"
+				@error = "Corrupted file: "+file
+			end
+			
+			#Check that all required data is extracted
+			if (@x_value==-1 || @y_value==-1  || @z_value==-1 || @volume==-1 )
+				@error = "Could not extract all data"
+			end
 		end
 	end
   
@@ -68,6 +96,11 @@ class StlExtract
 		#Return stored volume
 		@volume
 	end  
+
+	def error()
+		#Return stored volume
+		@error
+	end  	
 end
 
 
